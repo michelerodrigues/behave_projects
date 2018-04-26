4eea327f19da922d1517321a9cc87fbb5b9d184e
Michele da Silva Rodrigues <micheledasilva.rodrigues@gmail.com>
2018-04-26 16:11:03 -0300

    Changes on calculator funcionality

--- steps/calculator.py
+++ steps/calculator.py
@@ -3,9 +3,6 @@ class Calculator(object):
     def __init__(self, value=0):
         self.result = value
 
-    def reset(self):
-        self.result = 0
-
     def add2(self, x, y):
-        self.result += (x + y)
-        return self.result
+        result = x + y
+        return result
